198b0c63e106d82974f43bd11ad57296368c447b
zlat-k <64163122+zlat-k@users.noreply.github.com>
2020-10-04 02:34:34 +0300

    feat: upload js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arrBrackets = [];
+    let tmp = 0;
+    arrBrackets = bracketsConfig.map((item) => item.join('') );
+    while (tmp < arrBrackets.length) {
+        if (str.includes(arrBrackets[tmp])) {
+            str = str.replace(arrBrackets[tmp], '');
+            tmp = 0;
+        } else {
+            tmp++;
+        }
+    }if (str.length === 0){
+        return true
+    }
+    return false;
+
 }
